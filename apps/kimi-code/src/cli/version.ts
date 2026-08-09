bhjia-phys/aitp-agent

Use Kimi coding transport identity

apps/kimi-code/src/cli/version.ts
@@ -9,7 +9,10 @@ import { dirname, resolve } from 'node:path';
 
 import { createKimiDefaultHeaders, type KimiHostIdentity } from '@moonshot-ai/kimi-code-oauth';
 
-import { CLI_USER_AGENT_PRODUCT } from '#/constant/app';
+import {
+  KIMI_CODING_AGENT_USER_AGENT_PRODUCT,
+  KIMI_CODING_AGENT_USER_AGENT_SUFFIX,
+} from '#/constant/app';
 
 import { getDataDir } from '../utils/paths';
 import { KIMI_BUILD_INFO } from './build-info';
@@ -50,8 +53,9 @@ export function getVersion(): string {
 
 export function createKimiCodeHostIdentity(version = getVersion()): KimiHostIdentity {
   return {
-    userAgentProduct: CLI_USER_AGENT_PRODUCT,
+    userAgentProduct: KIMI_CODING_AGENT_USER_AGENT_PRODUCT,
     version,
+    userAgentSuffix: KIMI_CODING_AGENT_USER_AGENT_SUFFIX,
   };
 }
 

apps/kimi-code/test/cli/run-shell.test.ts
@@ -191,8 +191,9 @@ describe('runShell', () => {
     expect(mocks.kimiHarnessConstructor).toHaveBeenCalledWith(
       expect.objectContaining({
         identity: expect.objectContaining({
-          userAgentProduct: 'hakimi-cli',
+          userAgentProduct: 'kimi-code-cli',
           version: '1.2.3-test',
+          userAgentSuffix: 'hakimi',
         }),
       }),
     );

bhjia-phys/Hakimi

apps/kimi-code/src/cli/version.ts
@@ -9,7 +9,10 @@ import { dirname, resolve } from 'node:path';
 
 import { createKimiUserAgent, KIMI_CODE_PLATFORM, type KimiHostIdentity } from '@moonshot-ai/kimi-code-oauth';
 
-import { CLI_USER_AGENT_PRODUCT } from '#/constant/app';
+import {
+  KIMI_CODING_AGENT_USER_AGENT_PRODUCT,
+  KIMI_CODING_AGENT_USER_AGENT_SUFFIX,
+} from '#/constant/app';
 
 import { KIMI_BUILD_INFO } from './build-info';
 
@@ -49,9 +52,10 @@ export function getVersion(): string {
 
 export function createKimiCodeHostIdentity(version = getVersion()): KimiHostIdentity {
   return {
-    productName: CLI_USER_AGENT_PRODUCT,
+    productName: KIMI_CODING_AGENT_USER_AGENT_PRODUCT,
     version,
     platform: KIMI_CODE_PLATFORM,
+    userAgentSuffix: KIMI_CODING_AGENT_USER_AGENT_SUFFIX,
   };
 }
 

apps/kimi-code/test/cli/run-shell.test.ts
@@ -287,8 +287,9 @@ describe('runShell', () => {
     expect(mocks.kimiHarnessConstructor).toHaveBeenCalledWith(
       expect.objectContaining({
         identity: expect.objectContaining({
-          productName: 'hakimi-cli',
+          productName: 'kimi-code-cli',
           version: '1.2.3-test',
+          userAgentSuffix: 'hakimi',
         }),
         sessionStartedProperties: { yolo: true, auto: false, plan: true, afk: false },
       }),